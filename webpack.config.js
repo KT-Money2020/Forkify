22741b7fff63d10ca3085fa908e8d1161d1a12ae
Kevin <mai_le09@yahoo.com>
2020-03-25 23:14:54 -0500

    deleted useless files

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
+const path = require('path');
 
 module.exports = {
     entry: './src/js/index.js',
     output:{
-        path: '',
+        path: path.resolve(__dirname, 'dist/js'),
         filename:'bundle.js'
     }
-}
+} 
 
 
 
