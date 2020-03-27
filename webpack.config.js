700e888a99ef3a6bc325d1a19a684a48af5bf96f
Kevin <mai_le09@yahoo.com>
2020-03-27 02:35:18 -0500

    Working with Babel

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,18 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html'
         })
-    ]
+    ],
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude:/node_modules/,
+                use:{
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
 
 
