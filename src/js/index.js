2a4ba3a6c3a337344a6c1d4769a4993d5fe4d5e5
Kevin <mai_le09@yahoo.com>
2020-03-31 21:07:14 -0500

    back and forth recipe list functionality, ect

--- src/js/index.js
+++ src/js/index.js
@@ -17,7 +17,7 @@ const controlSearch = async () =>  {
         await state.search.getResults();
 
         clearLoader();
-        
+
         searchView.renderResults(state.search.result);
         
     }
@@ -30,7 +30,14 @@ elements.searchForm.addEventListener('submit', e=> {
     controlSearch();
 });
 
-
+elements.searchResPages.addEventListener('click', e=> {
+    const btn = e.target.closest('.btn-inline');
+    if(btn){
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
 
 
 
